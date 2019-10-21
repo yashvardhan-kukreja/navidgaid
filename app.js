ee05dbcfe7048000eac86cf6a4523a9abc12addc
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-10-21 20:49:21 +0530

    Added: Support for helmet and compression

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -10,12 +13,15 @@ const main_router = require("./main_router");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(helmet());
+app.use(compression());
+
 app.use(morgan("dev"));
 
 app.use("/api", main_router);
 
 app.get("/", (req, res) => {
-    res.send("yes");
+    res.send("Server's pretty healthy");
 });
 
 app.listen(port, () => console.log(`Server running successfully on port number: ${port}...`));
